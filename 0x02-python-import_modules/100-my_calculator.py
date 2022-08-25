9c6db7fea0db2eaeb164cb34d991d9790ebbbc93
Makuo67 <okekemakuo67@gmail.com>
2022-08-25 08:24:07 -0700

    module file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    nargs = len(sys.argv) -1
+    nargs = len(sys.argv) - 1
     if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
