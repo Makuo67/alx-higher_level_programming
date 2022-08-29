8162c5216195b02914c0d77614fcb810a302c957
Okeke Makuochukwu <99495812+Makuo67@users.noreply.github.com>
2022-08-29 20:49:10 +0200

    Update 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# Function that retrieves an element from a list like in C.
 def element_at(my_list, idx):
     if idx < 0:
         return (None)
-    if idx > len(my_list) - 1:
+
+    length = len(my_list)
+
+    if idx > length - 1:
         return (None)
 
     return(my_list[idx])
-
