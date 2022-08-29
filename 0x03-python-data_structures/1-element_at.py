14cf8caca9b2e7a17c2be5cb9c92a4dc1536637c
Makuo67 <okekemakuo67@gmail.com>
2022-08-29 11:46:02 -0700

    Data structure file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # Function that retrieves an element from a list like in C.
 def element_at(my_list, idx):
-    for idx in my_list:
-        if idx == -idx:
-            print("None")
-        elif idx > len(my_list):
-            print("None")
-        else:
-            print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+    if idx < 0:
+        return (None)
+    if idx > len(my_list) - 1:
+        return (None)
+
+    return(my_list[idx])
+
